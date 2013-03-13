df17a690b6122492f8eba5dbff7745efca03b12d
Alexander <alexandr.davljatov@gmail.com>
2013-03-13 15:16:11 +0400

    Everything is commited

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO User (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-1, 'jdoe', 'John', 'Doe');
+INSERT INTO User (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-2, 'emuster', 'Erika', 'Mustermann');
